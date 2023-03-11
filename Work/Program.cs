1641b03fd4cdca743ec16c7010409101e658a8ba
Quasar <puzyrkovv@yandex.ru>
2023-03-11 11:05:28 +0700

    Added two methods

--- Work/Program.cs
+++ Work/Program.cs
@@ -1,1 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿string[] NewStringArray(int length)
+{
+    string[] array = new string[length];
+    for(int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"Введите строку массива:\t");
+        array[i] = Console.ReadLine();
+    }
+    return array;
+}
+
+void ArrayShow (string[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.WriteLine(array[i]);
+
+}
+Console.Write("Укажите длину массива: ");
+int length = Convert.ToInt32(Console.ReadLine());
+string[] newArr = NewStringArray(length);
+ArrayShow(newArr);
